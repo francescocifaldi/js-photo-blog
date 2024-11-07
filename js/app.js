8a840625e2ba2f8a93e9457f02438ff812bd696f
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-07 11:05:33 +0100

    EDIT col as newEl + innerHTML

--- js/app.js
+++ js/app.js
@@ -1,39 +1,44 @@
 let num = prompt('Quante foto vuoi vedere?')
-const cardElem = document.getElementById("card-container");
+const row = document.getElementById("card-container");
 const btn = document.getElementById("regen")
 
 function generateList(num) {
-    cardElem.classList.add("d-none")
+    row.classList.add("d-none")
     axios
         .get(`https://jsonplaceholder.typicode.com/photos?_limit=${num}`)
         .then((res) => {
-            for (let i = 0; i < num; i++)
-                cardElem.innerHTML += `<div class="col-lg-4 col-md-6">
-                                            <div class="my-card h-100">
-                                                <img src="img/pin.svg" class="pin"></img>
-                                                <div class="img-container">
-                                                    <img src=${res.data[i].url} alt="">
-                                                </div>
-                                                <div class="description">
-                                                    <p>
-                                                        ${res.data[i].title}   
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>`
+            for (let i = 0; i < num; i++) {
+                const newCol = document.createElement('div')
+                newCol.classList.add('col', 'col-lg-4', 'col-md-6')
+                newCol.innerHTML += `
+                                    <div class="my-card h-100">
+                                        <img src="img/pin.svg" class="pin"></img>
+                                        <div class="img-container">
+                                            <img src=${res.data[i].url} alt="">
+                                        </div>
+                                        <div class="description">
+                                            <p>
+                                                ${res.data[i].title}   
+                                            </p>
+                                        </div>
+                                    </div>
+                                    `
+                row.append(newCol);
+            }
         })
         .catch((err) => {
             console.log('Errore')
         })
         .then(() => {
-            cardElem.classList.remove("d-none");
+            row.classList.remove("d-none");
+            console.log(document.querySelectorAll('.col'))
         })
 }
 
 generateList(num);
 
 btn.addEventListener("click", function () {
-    cardElem.innerHTML = '';
+    row.innerHTML = '';
     num = prompt('Quante email vuoi generare?')
     generateList(num)
 })
