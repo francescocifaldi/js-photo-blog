7428d06bb60680c0d28a7e6c100ef0dc07f3fd1b
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-09 16:13:06 +0100

    ADD specific functions

--- js/app.js
+++ js/app.js
@@ -8,27 +8,59 @@ const closeBtn = document.getElementById('close')
 const overlayElem = document.getElementById('overlay')
 const imageToShow = document.getElementById('img-show')
 const bodySelector = document.querySelector('body')
-let responseArray = []
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const num = picsToShow.value;
     generateList(num);
-    regenBtn.classList.remove("d-none")
-    formContainer.classList.add("d-none")
-    picsToShow.value = ''
 })
 
 function generateList(num) {
-    row.classList.add("d-none")
     axios
         .get(`https://jsonplaceholder.typicode.com/photos?_limit=${num}`)
         .then((res) => {
-            for (let i = 0; i < num; i++) {
-                responseArray.push(res.data[i])
-                const newCol = document.createElement('div')
-                newCol.classList.add('col-12', 'col-lg-4', 'col-md-6')
-                newCol.innerHTML += `
+            cardsGenerator(res, num)
+            cardsOpenerGenerator(res)
+            toggleForm()
+            picsToShow.value = ''
+        })
+        .catch((err) => {
+            console.log('Errore')
+        })
+}
+
+regenBtn.addEventListener("click", (event) => {
+    row.innerHTML = '';
+    toggleForm()
+})
+
+closeBtn.addEventListener('click', (event) => {
+    event.stopPropagation()
+    toggleOverlay()
+})
+
+overlayElem.addEventListener('click', (event) => {
+    if (event.target === overlayElem) {
+        toggleOverlay()
+    }
+})
+
+function toggleOverlay() {
+    overlayElem.classList.toggle('d-none')
+    bodySelector.classList.toggle('overflow-y-hidden')
+}
+
+function toggleForm() {
+    regenBtn.classList.toggle("d-none")
+    formContainer.classList.toggle("d-none")
+}
+
+function cardsGenerator(res, numbers) {
+    const fragment = document.createDocumentFragment()
+    for (let i = 0; i < numbers; i++) {
+        const newCol = document.createElement('div')
+        newCol.classList.add('col-12', 'col-lg-4', 'col-md-6')
+        newCol.innerHTML += `
                                     <div class="my-card h-100">
                                         <img src="img/pin.svg" class="pin"></img>
                                         <div class="img-container">
@@ -41,42 +73,17 @@ function generateList(num) {
                                         </div>
                                     </div>
                                     `
-                row.append(newCol);
-            }
-            console.log(responseArray)
-        })
-        .then(() => {
-            row.classList.remove("d-none");
-            regenBtn.classList.remove("d-none")
-            const elements = document.querySelectorAll('.my-card')
-            elements.forEach((element, i) => {
-                element.addEventListener('click', function () {
-                    imageToShow.innerHTML = `<img src=${responseArray[i].url} alt=""></img>`
-                    overlayElem.classList.remove('d-none')
-                    bodySelector.classList.add('overflow-y-hidden')
-                })
-            })
-        })
-        .catch((err) => {
-            console.log('Errore')
-        })
+        fragment.appendChild(newCol)
+    }
+    row.append(fragment);
 }
 
-regenBtn.addEventListener("click", (event) => {
-    row.innerHTML = '';
-    regenBtn.classList.add("d-none")
-    formContainer.classList.remove("d-none")
-})
-
-closeBtn.addEventListener('click', (event) => {
-    event.stopPropagation()
-    overlayElem.classList.add('d-none')
-    bodySelector.classList.remove('overflow-y-hidden')
-})
-
-overlayElem.addEventListener('click', (event) => {
-    if (event.target === overlayElem) {
-        overlayElem.classList.add('d-none')
-        bodySelector.classList.remove('overflow-y-hidden')
-    }
-})
+function cardsOpenerGenerator(res) {
+    const elements = document.querySelectorAll('.my-card')
+    elements.forEach((element, i) => {
+        element.addEventListener('click', function () {
+            imageToShow.innerHTML = `<img src=${res.data[i].url} alt="">`
+            toggleOverlay()
+        })
+    })
+}
